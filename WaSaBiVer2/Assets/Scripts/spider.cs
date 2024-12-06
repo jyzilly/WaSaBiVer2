bb7f2af36e4062f770188374af8135ddf92e19df
euijeong <euijeong20@naver.com>
2024-12-06 09:29:53 +0900

    no message

--- WaSaBiVer2/Assets/Scripts/spider.cs
+++ WaSaBiVer2/Assets/Scripts/spider.cs
@@ -7,12 +7,16 @@ public class spider : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
+    public Transform target;
+
+    private Animator animator;
     int m_CurrentWaypointIndex;
 
     private void Start()
     {
         navMeshAgent.SetDestination(waypoints[0].position);
-        
+        animator = GetComponent<Animator>();
+
     }
 
     private void Update()
@@ -24,5 +28,29 @@ public class spider : MonoBehaviour
             m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
             navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
         }
+        
     }
+
+    private void OnTriggerStay(Collider _collider) 
+    {
+        if (_collider.CompareTag("Player"))
+        {
+            //navMeshAgent.isStopped = true;
+            //animator.ResetTrigger("isAttack");
+            animator.SetBool("isWalk", false);
+            animator.SetBool("isAttack", true);
+            navMeshAgent.SetDestination(target.position);
+            //navMeshAgent.isStopped = false;
+        }
+
+        if(!(_collider.CompareTag("Player")))
+        {
+            animator.SetBool("isAttack", false);
+            animator.SetBool("isWalk", true);
+
+        }
+
+
+    }
+
 }
